43fd3a9b1ce034e5161c1c81c5eb3dc40c483683
Sneharikavallala <101981954+Sneharikavallala@users.noreply.github.com>
2022-06-06 23:00:29 +0530

    weather.js

---
+++ weather.js
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>weather</title>
+    <script src="weather.js"></script>
+    
+</head>
+<body>
+   
+        <input type="text" id="cityName" placeholder="Enter a city name"><br>
+        <input type="text" id="temperature" ><br>
+        <button onclick="fetchWeather()">Click</button>
+
+       <div id="display_data"></div>
+</body>
+</html>
